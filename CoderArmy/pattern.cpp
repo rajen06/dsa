69dd83963d6b8226d586d0f366466fa34ea2b92d
Raj <mukesh@Laptop334.local>
2024-04-13 20:21:52 +0530

    started

---
+++ CoderArmy/pattern.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// for (int i = 1; i <= 5; i++)
+	// {
+	// 	for (int j = 1; j <= 5; j++)
+	// 	{
+	// 		cout << i << " ";
+	// 	}
+	// 	cout << endl;
+	// }
+
+	// char c = 'a';
+	// for (int i = 1; i <= 5; i++)
+	// {
+	// 	for (int j = 1; j <= 5; j++)
+	// 	{
+	// 		cout << c << " ";
+	// 	}
+	// 	cout << endl;
+	// 	c++;
+	// }
+
+	// for (int i = 1; i <= 5; i++)
+	// {
+	// 	char c = 'a'+ i-1;
+	// 	for (int j = 1; j <= 5; j++)
+	// 	{
+	// 		cout << c << " ";
+	// 	}
+	// 	cout << endl;
+	// }
+
+	// for (char i = 'a'; i <= 'e'; i++)
+	// {
+
+	// 	for (char j = 'a'; j <= 'e'; j++)
+	// 	{
+	// 		cout << i << " ";
+	// 	}
+	// 	cout << endl;
+	// }
+
+	// for (char i = 'a'; i <= 'e'; i++)
+	// {
+
+	// 	for (char j = 'a'; j <= 'e'; j++)
+	// 	{
+	// 		cout << j << " ";
+	// 	}
+	// 	cout << endl;
+	// }
+
+	// int count =1;
+	// for (int i = 1; i <= 5; i++)
+	// {
+	// 	for (int j = 1; j <= 5; j++)
+	// 	{
+	// 		cout << count << " ";
+	// 		count++;
+	// 	}
+	// 	cout << endl;
+	// }
+
+	// for (int i = 1; i <= 5; i++)
+	// {
+	// 	for (int j = 1; j <= 5; j++)
+	// 	{
+	// 		cout << (i-1)*5+j << " ";
+	// 	}
+	// 	cout << endl;
+	// }
+}
---
+++ CoderArmy/sample.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Hello CA"<<endl;
+	cout<<"How are you all?"<<endl;
+	//variable declare
+	int a, b;
+	cout<<"Input for a:"<<endl;cin>>a;cout<<"Input for b:"<<endl;cin>>b;
+	cout<<a+b;
+
+}
